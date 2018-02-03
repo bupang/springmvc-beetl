6e0bcfd3c0c47ee33a81c5fcaae9efbcbf59c6a0
pine <chenbupang@126.com>
2018-02-03 19:24:04 +0800

    调试service

--- src/main/java/com/pine/moudles/sys/controller/TestController.java
+++ src/main/java/com/pine/moudles/sys/controller/TestController.java
@@ -1,5 +1,9 @@
 package com.pine.moudles.sys.controller;
 
+import com.baomidou.mybatisplus.mapper.EntityWrapper;
+import com.pine.moudles.sys.model.entity.auth.User;
+import com.pine.moudles.sys.service.auth.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -8,10 +12,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * Created by chenbupang on 2018-2-2 21:38
  */
 @Controller
+@RequestMapping("index")
 public class TestController {
-    @RequestMapping("/index")
+    @Autowired
+    private IUserService userService;
+
+    @RequestMapping("")
     @ResponseBody
-    public String testSpring(){
-        return "springmvc+mybatisplus";
+    public User index(){
+        return userService.selectOne(new EntityWrapper<User>().where("username={0}","admin"));
     }
 }
